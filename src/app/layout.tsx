ace9df191f947387f3ca0b8aa83a77a5fb8607bf
Luis Soriano <l.soriano@correo.unimet.edu.ve>
2025-04-03 19:20:16 -0400

    intentando solucionar fuente en movil

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,12 @@ import type { Metadata } from 'next';
 import './globals.css';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
+import "../../public/fonts/fonts.css"
 
 export const metadata: Metadata = {
   title: 'Quiropedistas Tamanaco - Especialistas en el cuidado de tus pies',
   description: 'Centro especializado en quiropedia, podología estética y tratamientos para el bienestar de tus pies en Caracas. Agenda tu cita hoy.',
-  keywords: 'quiropedia, podología, cuidado de pies, Caracas, tratamientos podológicos, salud de pies',
+  keywords: 'quiropedia, podología, cuidado de pies, Caracas, tratamientos podológicos, salud de pies, quiropedistas, quiropedia en caracas, podologos en caracas, productos para pies en caracas',
 };
 
 export default function RootLayout({
