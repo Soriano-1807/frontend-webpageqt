d1262b2e611b60e4a19dd3d61fc102b8a15426ed
Luis Soriano <l.soriano@correo.unimet.edu.ve>
2025-04-21 22:12:19 -0400

    poniendo bonito next.config.ts y creo que ya listo

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,7 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  // Comentamos temporalmente las redirecciones para solucionar el problema
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: "/:path*",
-  //       has: [
-  //         {
-  //           type: "host",
-  //           value: "www.quiropedistastamanaco.com",
-  //         },
-  //       ],
-  //       destination: "https://quiropedistastamanaco.com/:path*",
-  //       permanent: true,
-  //     },
-  //   ]
-  // },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
