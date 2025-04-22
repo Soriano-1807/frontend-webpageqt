6e8baf019dce0de05544c4bc55f457cf3eeb8f70
Luis Soriano <l.soriano@correo.unimet.edu.ve>
2025-04-21 22:02:52 -0400

    nop antes no fue resuelto. trabajando en eso

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,22 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.quiropedistastamanaco.com",
-          },
-        ],
-        destination: "https://quiropedistastamanaco.com/:path*",
-        permanent: true,
-      },
-    ]
-  },
+  // Comentamos temporalmente las redirecciones para solucionar el problema
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/:path*",
+  //       has: [
+  //         {
+  //           type: "host",
+  //           value: "www.quiropedistastamanaco.com",
+  //         },
+  //       ],
+  //       destination: "https://quiropedistastamanaco.com/:path*",
+  //       permanent: true,
+  //     },
+  //   ]
+  // },
   eslint: {
     ignoreDuringBuilds: true,
   },
